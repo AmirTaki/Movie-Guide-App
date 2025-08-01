6f281cb48a5fdfea8a3184bfbde47cb053fba6b1
amir <amirtaki623@gmail.com>
2025-08-01 21:46:25 +0330

    js part 2 at project

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 const movieNameRef = document.getElementById("move-name")
 const searchBtn = document.getElementById("search-btn")
 const result = document.getElementById("result")
+
+
+const getMovie = () => {
+    const movieName = movieNameRef.ariaValueMax;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+}
